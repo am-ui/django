446d8c199adc2c966709a78572c56188fdb4daab
Amit Kumar Pathak <pathakamitkumar67@gmail.com>
2021-07-26 12:03:02 +0530

    calc python

---
+++ cal/urls.py
@@ -0,0 +1,13 @@
+from os import name
+from django.contrib import admin
+from django.urls import path
+from django.urls.resolvers import URLPattern
+from . import views
+
+
+
+urlpatterns = [
+    path("", views.home, name="home"), 
+    path("add", views.add, name="add")
+    ]
+
